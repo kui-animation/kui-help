4e52e730421aaa160df155cf5e5c1fe25eb0538f
andrej <andrej.ciperle@gmail.com>
2025-07-28 18:25:08 +0200

    add types

--- inc/helper.h
+++ inc/helper.h
@@ -5,6 +5,17 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+typedef int_fast8_t t_int_8
+typedef int_fast16_t t_int_16
+typedef int_fast32_t t_int_32
+typedef int_fast64_t t_int_64
 
+typedef uint_fast8_t t_uint_8
+typedef uint_fast16_t t_uint_16
+typedef uint_fast32_t t_uint_32
+typedef uint_fast64_t t_uint_64
+
+typedef float t_float_32
+typedef double t_float_64
 
 #endif
